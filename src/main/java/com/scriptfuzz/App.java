9a3dae7c390339fdadf5ac4721da0ea7be9a143c
josediaz30 <jose.diaz30@upr.edu>
2016-12-07 00:35:00 -0400

    Deployable in outside tomcat

--- src/main/java/com/scriptfuzz/App.java
+++ src/main/java/com/scriptfuzz/App.java
@@ -2,16 +2,23 @@ package com.scriptfuzz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 @EnableAutoConfiguration
-public class App {
+public class App extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(App.class);
     }
 
     @RequestMapping("/")
